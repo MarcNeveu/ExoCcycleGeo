e78f0faeb0ccc4217fdc2f01e03b4004218b123a
Marc Neveu <nifiou@gmail.com>
2022-02-22 21:38:51 -0500

    ExoCcycleGeo v22.2.23
    Added low tolerance threshold to v22.2.22 change so as to avoid negative
    values of xgas resulting from machine error.

--- ExoCcycleGeo/src/ExoCcycleGeo.c
+++ ExoCcycleGeo/src/ExoCcycleGeo.c
@@ -803,12 +803,12 @@ int main(int argc, char *argv[]) {
 
 		// Redox of outgassing (see e.g. Gaillard and Scaillet 2014 EPSL or Scaillet and Gaillard 2011 Nature comment)
 		if (redox <= 4) {
-			if (xgas[0]*nAir + 1.0*dtime*netFC < 0.0) netFC = -xgas[0]*nAir/(1.0*dtime);
+			if (xgas[0]*nAir + 1.0*dtime*netFC < 0.0) netFC = -xgas[0]*nAir/(1.0*dtime)*(1.0-1.0e-8);
 			xgas[0] = (xgas[0]*nAir + 1.0*dtime*netFC)/(nAir + dtime*netFC); // CO2 dominates over CH4, assume 100% added gas is CO2 and let equilibration with ocean speciate accurately
 			xgas[1] = xgas[1]*nAir/(nAir + dtime*netFC);                     // Dilute CH4 (or concentrate if netFC < 0)
 		}
 		else {
-			if (xgas[1]*nAir + 1.0*dtime*netFC < 0.0) netFC = -xgas[1]*nAir/(1.0*dtime);
+			if (xgas[1]*nAir + 1.0*dtime*netFC < 0.0) netFC = -xgas[1]*nAir/(1.0*dtime)*(1.0-1.0e-8);
 			xgas[1] = (xgas[1]*nAir + 1.0*dtime*netFC)/(nAir + dtime*netFC); // CH4 dominates over CO2, assume 100% added gas is CH4
 			xgas[0] = xgas[0]*nAir/(nAir + dtime*netFC);                     // Dilute CO2 (or concentrate if netFC < 0)
 		}
@@ -1382,10 +1382,10 @@ int main(int argc, char *argv[]) {
 			// Today indicative size of 7 major plates is 70e6 km2, corresponding to diameter 2*sqrt(70e6/(4*pi)) = 4720 km > mantle depth, even though (isoviscous) convection cell should have aspect ratio of 1 (Bercovici et al. 2015).
 			// For Earth inputs it is = mantle depth (2900 km) for Ra = 1e7, i.e., 2 billion years ago (oldest evidence of plate tectonics 2-3 Ga; Brown et al. 2020)
 
-//			zCrack = realtime/Myr2sec; // Reflects secular cooling below crust, which prevents cracks from healing as fast
+			zCrack = realtime/Myr2sec; // Reflects secular cooling below crust, which prevents cracks from healing as fast
 
-			volSeafCrust = (1.0-L)/(1.0-0.29) * LplateBnd*vConv*zCrack*dtime; // MOR length = 6.5*Earth circumference, crust assumed fully cracked
-//			volSeafCrust = (1.0-L)/(1.0-0.29) * LplateBnd*(vConv*fmin(1.0,realtime/(1.5*Gyr2sec)))*zCrack*dtime; // vConv*fmin() term represents sluggish convection until full plate tectonics
+//			volSeafCrust = (1.0-L)/(1.0-0.29) * LplateBnd*vConv*zCrack*dtime; // MOR length = 6.5*Earth circumference, crust assumed fully cracked
+			volSeafCrust = (1.0-L)/(1.0-0.29) * LplateBnd*(vConv*fmin(1.0,realtime/(1.5*Gyr2sec)))*zCrack*dtime; // vConv*fmin() term represents sluggish convection until full plate tectonics
 
 			Pseaf = rhoH2O * g[NR] * (r_p - pow(pow(r_p,3) - Mocean/rhoH2O/(4.0/3.0*PI_greek),1.0/3.0)) / bar2Pa; // Seafloor hydrostatic pressure: density*surface gravity*ocean depth TODO scale with land coverage
 			WRseafW = Mocean*dtime/tcirc / volSeafCrust; // Will be multiplied in AqueousChem() by (mass rock input to PHREEQC / seafloor crust density) = volume rock input to PHREEQC. 1e7 yr is hydrothermal circulation timescale (Kadko et al. 1995)
